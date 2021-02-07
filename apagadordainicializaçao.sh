502cae01a719b8c3ee44db1ae442762c8de8390b
KILANERROR404 <70495592+KILANERROR404@users.noreply.github.com>
2021-02-07 02:38:34 -0400

    Create apagadordainicializaçao.sh

---
+++ apagadordainicializaçao.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+#PROGAMADO POR KILAN
+#creditos: kilan
+
+clear
+      figlet INSTALANDO...
+ pkg install ruby
+       gem install lolcat
+      pkg install nano
+  figlet INSTALADO | lolcat
+echo 'INICIANDO FERRAMENTA' | lolcat 
+
+echo "CANAL DO CRIADOR DA FERRAMENTA"
+
+    cd ..
+ cd ..
+    ls
+  cd usr
+ cd etc
+ls
+  nano motd
+  #copyright SE ROUBAR MEU PROJETO OU COPIAR ELE.
+  
